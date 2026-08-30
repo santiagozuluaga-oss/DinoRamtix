2ecb743c0fc2b0ea7599fff0c1fbb95fe5fee34a
santiagozuluaga-oss <santiago.zuluaga@fundaciongpl.org>
2026-08-30 11:05:12 -0500

    Refresh DinoRamtix PWA cache for new icon and startup animation

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE='dinoramtix-shell-v1';
-const SHELL=['/','/index.html','/config.js','/future-ui.js','/games-v3.js','/manifest.webmanifest'];
+const CACHE='dinoramtix-shell-v2';
+const SHELL=['/','/index.html','/config.js','/future-ui.js','/games-v3.js','/startup.js','/manifest.webmanifest','/icon.svg?v=20260830'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',event=>event.waitUntil(self.clients.claim()));
+self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k.startsWith('dinoramtix-shell-')&&k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{if(response.ok&&new URL(event.request.url).origin===location.origin){const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy))}return response}).catch(()=>cached)))});
